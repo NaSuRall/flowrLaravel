b19c1a8031543d538dd522a156523b0e3900fb14
Your Name <romain.poulain@edu.esiee_it.fr>
2025-03-05 11:26:36 +0100

    Add All listes on grp

---
+++ database/factories/ListeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Group;
+
+class ListeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'user_id' => 1,
+            'group_id' => Group::inRandomOrder()->first()->id,
+        ];
+    }
+}
